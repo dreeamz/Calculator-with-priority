082a9d9eaf6ac01f65661d447054e56daf564f53
rustam-ahmadov <rustam.ahmadov114@gmail.com>
2022-09-13 20:14:56 +0400

    Complete calculate func

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 
-
 void fillExpressionBuffer(char *buffer)
 {
 	std::cin.getline(buffer, 250);
 }
 bool isValidSymbol(char sym)
 {
-	if (sym >= 48 && sym <= 57 || sym >= 40 && sym <= 43 || sym == 45 || sym == 47)
+	// 0 - 10 , ( ) * + , - , /
+	if (sym >= '0' && sym <= '9' || sym >= '(' && sym <= '+' || sym == '-' || sym == '/')
 		return true;
 	return false;
 }
-//  time O(n) 
+int getIntFromAsci(char sym)
+{
+	if (sym >= 48 && sym <= 57)
+		return sym - 48;
+	return -1;
+}
+//  time O(n)
 // space O(n)
 char *getMathExpressionFromBuffer(char *buffer, int m_expression_size)
 {
 	char *math_expression = new char[m_expression_size + 1];
 
-	for (int m_i = 0,i = 0; m_i < m_expression_size + 1 && buffer[i] != 0; i++)
-		if (isValidSymbol(buffer[i])){
+	for (int m_i = 0, i = 0; m_i < m_expression_size + 1 && buffer[i] != 0; i++)
+		if (isValidSymbol(buffer[i]))
+		{
 			math_expression[m_i] = buffer[i];
 			m_i++;
 		}
 
 	return math_expression;
 }
-//O(n)
+// O(n)
 int getMathExpressionSizeFromBuffer(char *buffer)
 {
 	int i = 0, size = 0;
@@ -37,17 +44,38 @@ int getMathExpressionSizeFromBuffer(char *buffer)
 	return size;
 }
 
-int calculateExpression(char *)
+int calculateMathExpression(char *m_expression)
 {
+	if (m_expression == nullptr)
+		return 0;
 
+	int result = getIntFromAsci(m_expression[0]);
+	int temp = 0;
+	char operation = ' ';
+
+	for (int i = 0; m_expression[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			temp = getIntFromAsci(m_expression[i]);
+			if (operation == '*')
+				result *= temp;
+			else if (operation == '/')
+				result /= temp;
+		}
+		else
+			operation = m_expression[i];
+	}
+	return result;
 }
 
-int getMathExpressionResult(char *m_expression)
+int partitionMathExpression(char *m_expression)
 {
-	for(int i = 0; m_expression[i]!='\0';i++)
+	for (int i = 0; m_expression[i] != '\0'; i++)
 	{
 		
 	}
+	return 0;
 }
 
 int main()
@@ -61,5 +89,6 @@ int main()
 
 	std::cout << math_expression;
 	std::cout << '\n';
+	std::cout << "Unswer :" << calculateMathExpression(math_expression)<<std::endl;
 	return 0;
 }
